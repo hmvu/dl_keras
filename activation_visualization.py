617450b95b3e0ddbae6112475090091e8e16183f
hoangvu <hoangvu@hotmail.com>
2018-03-06 10:09:05 +0700

    add filters_visulization.py 16:18 5/2/18

--- activation_visualization.py
+++ activation_visualization.py
@@ -9,13 +9,14 @@ import numpy as np
 
 img = image.load_img(img_path, target_size=(150, 150))
 img_tensor = image.img_to_array(img)
+#print(img_tensor.shape)
 img_tensor = np.expand_dims(img_tensor, axis=0)
 img_tensor /= 255.
-#print(img_tensor.shape)
-import matplotlib.pyplot as plt
 
-#plt.imshow(img_tensor[0])
-#plt.show()
+import matplotlib.pyplot as plt
+#
+# plt.imshow(img_tensor[0])
+# plt.show()
 # create a Keras Model
 from keras import  models
 layer_outputs = [layer.output for layer in model.layers[:8]]
@@ -23,7 +24,7 @@ activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
 activations = activation_model.predict(img_tensor)
 # first_layer_activation = activations[0]
 # print(first_layer_activation.shape)
-# plt.matshow(first_layer_activation[0,:,:,8], cmap = 'viridis')
+# plt.matshow(first_layer_activation[0,:,:,6], cmap = 'viridis')
 # plt.show()
 # show all the activation layer
 layer_names = []
